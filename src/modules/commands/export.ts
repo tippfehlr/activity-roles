a035d9daa10642fe52087614244f6a859230aa08
tippfehlr <jonigrot444@gmail.com>
2022-02-22 18:00:38 +0100

    add /export to export all game roles as json

---
+++ src/modules/commands/export.ts
@@ -0,0 +1,29 @@
+import { ICommand } from 'wokcommands'
+import { table } from 'table';
+import fs from 'fs';
+
+import config from '../../../config';
+import msg from '../messages';
+import * as db from '../db';
+
+export default {
+  names: 'export',
+  category: 'Information',
+  description: 'Exports all game roles in your guild as a JSON file.',
+
+  slash: true,
+  testOnly: config.debug,
+
+  callback: async command => {
+    msg.log.activity();
+
+    const res: db.GuildDataType[] = await db.GuildData.find({ guildID: command.guild?.id })
+    let array = [];
+    for (const i in res) {
+      array.push([String(Number(i) + 1), `${command.guild?.roles.cache.find((role) => role.id === res[i].roleID)?.name} <@&${res[i].roleID}>`, res[i].activityName, res[i].exactActivityName.toString()]);
+    }
+    fs.writeFileSync(config.exportFileName, JSON.stringify(array, null, 1));
+    await command.interaction.reply({ files: [config.exportFileName] })
+    fs.unlink(config.exportFileName, () => { })
+  }
+} as ICommand
