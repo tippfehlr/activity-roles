26a54a7a436eaea17193a09beff93481c0a4ef25
tippfehlr <tippfehlr@gmail.com>
2022-11-24 10:48:15 +0100

    add /stats command & display stats in activity [closes #31]

---
+++ src/modules/commands/stats.ts
@@ -0,0 +1,20 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { Command } from '../commandHandler';
+import { db } from '../db';
+import msg from '../messages';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('stats')
+    .setDescription('Shows some stats about the bot.'),
+  execute: async interaction => {
+    interaction.reply({
+      embeds: [
+        msg.statsEmbed(
+          interaction.client.guilds.cache.size,
+          db.prepare('SELECT COUNT(*) FROM users').get()['COUNT(*)']
+        )
+      ]
+    });
+  }
+} as Command;
