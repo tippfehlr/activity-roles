1cca6707706a549eb317816199e0273bd61cba63
tippfehlr <tippfehlr@gmail.com>
2023-10-11 23:29:41 +0200

    ⚡ add database auto-migration (version number in table dbversion)

--- src/modules/db.ts
+++ src/modules/db.ts
@@ -2,6 +2,7 @@ import fs from 'fs'
 import sqlite3 from 'better-sqlite3';
 import { createHash } from 'crypto';
 import { CommandInteraction, Locale, StringSelectMenuInteraction } from 'discord.js';
+import { log } from './messages';
 
 export interface DBUser {
   userIDHash: string;
@@ -32,27 +33,39 @@ export interface DBActivityStats {
   count: number;
 }
 
+export interface DBVersion {
+  version: number;
+  enforcer: 0;
+}
+
 export let db: sqlite3.Database;
 
 export function prepareDB() {
   if (!fs.existsSync('db')) fs.mkdirSync('db');
   db = new sqlite3('db/activity-roles.db');
 
-  db.prepare(
-    'CREATE TABLE IF NOT EXISTS users (userIDHash TEXT PRIMARY KEY, autoRole INTEGER, language TEXT)'
-  ).run();
-  db.prepare(
-    'CREATE TABLE IF NOT EXISTS guilds (guildID TEXT PRIMARY KEY, language TEXT, requiredRoleID TEXT)'
-  ).run();
-  db.prepare(
-    'CREATE TABLE IF NOT EXISTS activityRoles (guildID TEXT, activityName TEXT, roleID TEXT, exactActivityName INTEGER, live INTEGER, PRIMARY KEY (guildID, activityName, roleID))'
-  ).run();
-  db.prepare(
-    'CREATE TABLE IF NOT EXISTS currentlyActiveActivities (userIDHash TEXT, guildID TEXT, activityName TEXT, PRIMARY KEY (userIDHash, guildID, activityName))'
-  ).run();
-  db.prepare(
-    'CREATE TABLE IF NOT EXISTS activityStats (guildID TEXT, activityName TEXT, count INTEGER, PRIMARY KEY (guildID, activityName))'
-  ).run();
+  // enforcer: see https://stackoverflow.com/a/3010975/16292720 (comment 4)
+  db.prepare('CREATE TABLE IF NOT EXISTS dbversion (version INT NOT NULL, enforcer INT DEFAULT 0 NOT NULL CHECK(enforcer == 0), UNIQUE (enforcer))').run();
+  db.prepare('CREATE TABLE IF NOT EXISTS users (userIDHash TEXT PRIMARY KEY, autoRole INTEGER, language TEXT)').run();
+  db.prepare('CREATE TABLE IF NOT EXISTS guilds (guildID TEXT PRIMARY KEY, language TEXT, requiredRoleID TEXT)').run();
+  db.prepare('CREATE TABLE IF NOT EXISTS activityRoles (guildID TEXT, activityName TEXT, roleID TEXT, exactActivityName INTEGER, live INTEGER, PRIMARY KEY (guildID, activityName, roleID))').run();
+  db.prepare('CREATE TABLE IF NOT EXISTS currentlyActiveActivities (userIDHash TEXT, guildID TEXT, activityName TEXT, PRIMARY KEY (userIDHash, guildID, activityName))').run();
+  db.prepare('CREATE TABLE IF NOT EXISTS activityStats (guildID TEXT, activityName TEXT, count INTEGER, PRIMARY KEY (guildID, activityName))').run();
+
+  if (!db.prepare('SELECT * FROM dbversion').get()) {
+    db.prepare('INSERT INTO dbversion (version) VALUES (1)').run();
+  }
+
+  const latestDBVersion = 1;
+  const dbVersion = (db.prepare('SELECT * FROM dbversion').get() as DBVersion).version;
+
+  //TODO: add bot version?
+  if (dbVersion > latestDBVersion) {
+    log.warn(`Database version: ${dbVersion}. The latest known database version is ${latestDBVersion}! Are you opening a database created with a newer version?`);
+  } else {
+    log.info(`Database version: ${dbVersion}`);
+  }
+
 }
 
 export function getUserConfig(userID: string): DBUser {
