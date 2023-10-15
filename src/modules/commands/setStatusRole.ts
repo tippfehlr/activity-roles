7e7abe524307abc1d7e15428020db9145d00f9c7
tippfehlr <tippfehlr@gmail.com>
2023-10-15 17:32:29 +0200

    feat(commands): add /setstatusrole
    close #43

---
+++ src/modules/commands/setStatusRole.ts
@@ -0,0 +1,125 @@
+import { DBStatusRole, db, getLang, getUserConfig } from './../db';
+import { Command } from '../commandHandler';
+
+import { __, __h_dc, getEnumKey } from '../messages';
+import { ActivityType, Colors, EmbedBuilder, SlashCommandBuilder, codeBlock } from 'discord.js';
+export default {
+  data: new SlashCommandBuilder()
+    .setName('setstatusrole')
+    .setDescription('role to assign on LISTENING/WATCHING/etc.')
+    .setDescriptionLocalizations(__h_dc('role to assign on LISTENING/WATCHING/etc.'))
+    .addStringOption(option =>
+      option
+        .setName('event')
+        .setDescription('the event the user must have to receive the role')
+        .setDescriptionLocalizations(__h_dc('the event the user must have to receive the role'))
+        .setChoices(
+          { name: 'Playing', name_localizations: __h_dc('Playing'), value: '0' }, // value 0
+          { name: 'Streaming', name_localizations: __h_dc('Streaming'), value: '1' }, // value 1
+          { name: 'Listening', name_localizations: __h_dc('Listening'), value: '2' }, // value 2
+          { name: 'Watching', name_localizations: __h_dc('Watching'), value: '3' }, // value 3
+          { name: 'Custom', name_localizations: __h_dc('Custom'), value: '4' }, // value 4
+          { name: 'Competing', name_localizations: __h_dc('Competing'), value: '5' } // value 5
+        )
+        .setRequired(true)
+    )
+    .addRoleOption(option =>
+      option
+        .setName('role')
+        .setDescription('the role to assign. To remove the role, omit this option')
+        .setDescriptionLocalizations(
+          __h_dc('the role to assign. To remove the role, omit this option')
+        )
+    ),
+
+  execute: async interaction => {
+    const locale = getLang(interaction);
+    const type = Number(interaction.options.get('event')?.value) as number;
+    const typeString = getEnumKey(ActivityType, type);
+    const role = interaction.options.get('role')?.role;
+    const currentStatusRole = db
+      .prepare('SELECT * FROM statusRoles WHERE guildID = ? and type = ?')
+      .get(interaction.guildId, type) as DBStatusRole | undefined;
+
+    if (role) {
+      if (currentStatusRole && role.id === currentStatusRole.roleID) {
+        interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(
+                __(
+                  {
+                    phrase: 'the status role for **%s** already is <@&%s>!',
+                    locale
+                  },
+                  typeString,
+                  role.id
+                )
+              )
+              .setColor(Colors.Green)
+          ],
+          ephemeral: true
+        });
+      } else {
+        if (currentStatusRole) {
+          db.prepare('UPDATE statusRoles SET roleID = ? WHERE guildID = ? AND type = ? ').run(
+            role.id,
+            interaction.guildId,
+            type
+          );
+        } else {
+          db.prepare('INSERT INTO statusRoles (guildID, type, roleID) VALUES (?, ?, ?)').run(
+            interaction.guildId,
+            type,
+            role.id
+          );
+        }
+        interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(
+                __(
+                  {
+                    phrase: 'The status role for **%s** is now <@&%s>!',
+                    locale
+                  },
+                  typeString,
+                  role.id
+                )
+              )
+              .setColor(Colors.Green)
+          ],
+          ephemeral: true
+        });
+      }
+    } else {
+      if (currentStatusRole) {
+        db.prepare('DELETE FROM statusRoles WHERE guildID = ? AND type = ?').run(
+          interaction.guildId,
+          type
+        );
+        interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(
+                __({ phrase: 'The status role for **%s** has been deleted.', locale }, typeString)
+              )
+              .setColor(Colors.Red)
+          ],
+          ephemeral: true
+        });
+      } else {
+        interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(
+                __({ phrase: 'There is no status role set for **%s**', locale }, typeString)
+              )
+              .setColor(Colors.Red)
+          ],
+          ephemeral: true
+        });
+      }
+    }
+  }
+} as Command;
