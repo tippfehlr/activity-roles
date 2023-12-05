9955be72d790f36f351d5375cff2ed533e7eb710
tippfehlr <tippfehlr@gmail.com>
2023-12-05 22:39:55 +0100

    fix(config): update invite link permissions

--- src/modules/config.ts
+++ src/modules/config.ts
@@ -4,7 +4,7 @@ import { log } from './messages';
 const missingEnv = (env: string) => {
   log.error(`Environment variable ${env} not provided.`);
   process.exit(1);
-}
+};
 
 const TOKEN = process.env.TOKEN;
 if (!TOKEN) missingEnv('TOKEN');
@@ -15,12 +15,12 @@ if (!APPLICATION_ID) missingEnv('APPLICATION_ID');
 const HOSTER_NAME = process.env.HOSTER_NAME;
 if (!HOSTER_NAME) missingEnv('HOSTER_NAME');
 
-const INVITE_LINK = `https://discord.com/api/oauth2/authorize?client_id=${APPLICATION_ID}&permissions=8&scope=bot%20applications.commands`
+const INVITE_LINK = `https://discord.com/api/oauth2/authorize?client_id=${APPLICATION_ID}&permissions=268435456&scope=bot%20applications.commands`;
 const COLOR = (process.env.COLOR || '#3695d3') as ColorResolvable;
 const SUPPORT_GUILD_LINK = 'https://discord.gg/3K9Yx4ufN7';
 const AUTHOR_LOGO_LINK = 'https://drive.google.com/uc?id=1c-jP8Znqm72U2kEhoOdP-DzFvpiyJnvd';
 const AUTHOR = 'tippfehlr#3575';
-const GUILD: string | false = process.env.GUILD || false
+const GUILD: string | false = process.env.GUILD || false;
 
 export default {
   TOKEN: TOKEN as string,
@@ -32,4 +32,4 @@ export default {
   AUTHOR_LOGO_LINK,
   AUTHOR,
   GUILD
-}
+};
