ee891f054cc57ee9e8d8d7b5f27da904ea1a3df5
tippfehlr <tippfehlr@tippfehlr.eu>
2024-04-10 19:45:35 +0200

    remove: remove Estonian
    The language is not supported by discord, therefore there is no way to
    get these translations.

--- src/modules/messages.ts
+++ src/modules/messages.ts
@@ -1,12 +1,6 @@
 import { pino } from 'pino';
 import { I18n } from 'i18n';
 
-export const locales = ['en-US', 'cs', 'nl', 'pt-BR', 'de', 'ru', 'uk', 'et-EE', 'fr'];
-
-// Estonian is not supported by discord and therefore can’t be used for command descriptions.
-const discordNotSupported = ['et-EE'];
-
-// as long as all locales from const locales are defined here, this is safe.
 export const localesMap: { [locale: string]: string } = {
   'en-US': 'American English',
   cs: 'Čeština (Czech)',
@@ -15,9 +9,9 @@ export const localesMap: { [locale: string]: string } = {
   de: 'Deutsch (German)',
   ru: 'Русский (Russian)',
   uk: 'Укра (Ukrainian)',
-  'et-EE': 'Eesti (Estonian)',
   fr: 'Français (French)',
 };
+export const locales = Object.keys(localesMap);
 
 export const i18n = new I18n({
   locales,
@@ -31,9 +25,7 @@ export function discordTranslations(s: string) {
   let dcTranslations: { [key: string]: string } = {};
 
   i18n.__h(s).forEach(language => {
-    if (!discordNotSupported.includes(Object.keys(language)[0])) {
-      dcTranslations = { ...dcTranslations, ...language };
-    }
+    dcTranslations = { ...dcTranslations, ...language };
   });
   return dcTranslations;
 }
