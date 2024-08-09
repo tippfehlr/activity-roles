87108873b8915ac932c0ca789a17fc24427bdea2
tippfehlr <tippfehlr@tippfehlr.eu>
2024-08-09 12:56:12 +0200

    chore: move scripts to package.json and release.fish
    Previously the scripts were in a private justfile. I moved passwords into
    .env files and scripts into package.json (and release.fish for release).

---
+++ release.fish
@@ -0,0 +1,31 @@
+#!/usr/bin/env fish
+
+set oldversion (cat package.json | jq -r .version)
+set newversion (git cliff --bumped-version | cut -c 2-)
+
+if [ "$argv" != "" ]
+    set newversion "$argv"
+end
+
+echo "$oldversion -> $newversion"
+
+git cliff --bump -o CHANGELOG.md
+sed -i "s/$oldversion/$newversion/" package.json
+git add CHANGELOG.md package.json
+git commit -m "chore(release): v$newversion"
+git tag v$newversion
+read -p "> Push and release changes?"
+git push
+git push --tags origin v$newversion
+
+# changelog for discord (via webhook)
+git cliff -l \
+    | sed 's/# Changelog//' \
+    | sed 's/## \[/# \[/' \
+    | gh release create v$newversion -F -
+
+# changelog for github
+git cliff -l \
+    | sed 's/# Changelog/## Changelog/' \
+    | rg -v '## \[' \
+    | gh release edit v$newversion -F -
